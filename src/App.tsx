1407ec6edc7d08bc9570060f1a8823629756977d
Logan Darby <logandarby4@gmail.com>
2025-06-25 17:59:17 -0400

    Set up hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import type { JSX } from 'react';
 
 interface LinkItem {
@@ -10,87 +10,77 @@ interface LinkItem {
 }
 
 function App() {
+  const ListenComponent = <div>Listen</div>;
 
-  const ListenComponent = (
-    <div>Listen</div>
-  );
+  const AboutComponent = <div>About</div>;
 
-  const AboutComponent = (
-    <div>About</div>
-  )
+  const ContactComponent = <div>Contact</div>;
 
-  const ContactComponent = (
-    <div>Contact</div>
-  )
-
-  const MoreComponent = (
-    <div>More</div>
-  )
+  const MoreComponent = <div>More</div>;
 
   const LINK_ITEMS: LinkItem[] = [
     {
-      label: "Listen",
-      image: "1.jpg",
+      label: 'Listen',
+      image: '1.jpg',
       component: ListenComponent,
       style: {
-        backgroundPosition: "100% 30%"
+        backgroundPosition: '100% 30%',
       },
     },
     {
-      label: "About",
-      image: "2.png",
+      label: 'About',
+      image: '2.png',
       component: AboutComponent,
       style: {
-        backgroundPosition: "-10% 25%",
+        backgroundPosition: '-10% 25%',
         backgroundSize: 500,
       },
     },
     {
-      label: "Contact",
-      image: "3.jpg",
+      label: 'Contact',
+      image: '3.jpg',
       component: ContactComponent,
       style: {
-        backgroundPosition: "80% 10%",
+        backgroundPosition: '80% 10%',
         backgroundSize: 930,
       },
     },
     {
-      label: "More",
-      image: "4.jpg",
+      label: 'More',
+      image: '4.jpg',
       component: MoreComponent,
       style: {
-        backgroundPosition: "-300% 48%",
+        backgroundPosition: '-300% 48%',
         backgroundSize: 900,
       },
-    }
+    },
   ];
 
   return (
     <>
-    <main>
-      <div className='flex flex-row h-screen w-screen'>
-        <div className="w-[60%] flex flex-col justify-around">
-          {LINK_ITEMS.map((item) => (
-            <div
-              className="link-item flex object-cover justify-center items-center font-propaganda text-white h-full relative"
-              style={{
-                ...item.style,
-                backgroundImage: `url(/img/${item.image})`,
-              }}
-              
-            >
-              <div className="link-item-component opacity-0">{item.component}</div>
-              <div className='link-item-label text-8xl'>
-                {item.label}
+      <main>
+        <div className="flex flex-row h-screen w-screen">
+          <div className="w-[60%] flex flex-col justify-around">
+            {LINK_ITEMS.map((item) => (
+              <div
+                className="link-item flex object-cover justify-center items-center font-propaganda text-white h-full relative"
+                style={{
+                  ...item.style,
+                  backgroundImage: `url(/img/${item.image})`,
+                }}
+              >
+                <div className="link-item-component opacity-0">
+                  {item.component}
+                </div>
+                <div className="link-item-label text-8xl">{item.label}</div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div>Col2</div>
         </div>
-        <div>Col2</div>
-      </div>
-    </main>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
